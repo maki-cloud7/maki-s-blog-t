475155b352d2c4be8df2cf3e9dee4c6c55b263fe
maki <maki@makideMacBook-Air.local>
2026-06-22 10:30:34 +0800

    Auto deploy from Local CMS

--- social.js
+++ social.js
@@ -1,6 +1,6 @@
 export const social = {
-  "github": "https://github.com/maki-cloud7#",
-  "bilibili": "#https://space.bilibili.com/2140415206?spm_id_from=333.1007.0.0",
+  "github": "https://github.com/maki-cloud7",
+  "bilibili": "https://space.bilibili.com/2140415206?spm_id_from=333.1007.0.0",
   "qq": "#",
-  "telegram": "#t.me/maki_fff"
+  "telegram": "t.me/maki_fff"
 };
