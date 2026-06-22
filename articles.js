68036b4a1dcc8ef342fb5e6c17b9c9e3d4cdf449
maki <maki@makideMacBook-Air.local>
2026-06-22 12:20:06 +0800

    Auto deploy from Local CMS

--- articles.js
+++ articles.js
@@ -1,1 +1,13 @@
-export const articles = [];
+export const articles = [
+  {
+    "id": "1782102000762",
+    "title": "我的第一篇文章",
+    "date": "2026-06-22",
+    "tags": [
+      "生活"
+    ],
+    "summary": "终于有了自己的博客了",
+    "url": "/post.html?id=1782102000762",
+    "image": "/uploads/1782101851326---2.png"
+  }
+];
