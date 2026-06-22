4c085122c9b0e314aaeff3c15a4b827ba3402be1
maki <maki@makideMacBook-Air.local>
2026-06-22 10:59:50 +0800

    Auto deploy from Local CMS

--- friends.js
+++ friends.js
@@ -1,16 +1,23 @@
 export const friends = [
   {
-    id: 1,
-    name: "Alice的茶会",
-    desc: "一位喜欢前端和魔法的少女。",
-    url: "#",
-    avatar: "/avatar1.jpg"
+    "id": 1,
+    "name": "Alice的茶会",
+    "desc": "一位喜欢前端和魔法的少女。",
+    "url": "#",
+    "avatar": "/avatar1.jpg"
   },
   {
-    id: 2,
-    name: "天空之城",
-    desc: "记录日常与技术的碎碎念。",
-    url: "#",
-    avatar: "/avatar2.jpg"
+    "id": 2,
+    "name": "天空之城",
+    "desc": "记录日常与技术的碎碎念。",
+    "url": "#",
+    "avatar": "/avatar2.jpg"
+  },
+  {
+    "id": "1782097186489",
+    "name": "一个栗子",
+    "desc": "新时代agent全栈攻城狮, 依旧未来可期",
+    "url": "https://iucoder99.github.io/blog_pages/",
+    "avatar": "/uploads/1782097184363-default.jpg"
   }
 ];
