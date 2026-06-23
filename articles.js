176d47a2d6fa4fb1d27c0bb756cfd3f1e74845df
maki <riook07h@gmail.com>
2026-06-23 14:21:48 +0800

    Update article meta 1782195704281

--- articles.js
+++ articles.js
@@ -1,5 +1,16 @@
 export const articles = [
   {
+    "id": "1782195704281",
+    "title": "测试一下行不行",
+    "date": "2026-06-23",
+    "tags": [
+      "测试"
+    ],
+    "summary": "就这么测试一下",
+    "url": "/post.html?id=1782195704281",
+    "image": "/images/1782195672016_【哲风壁纸】win11娘-Windows.png"
+  },
+  {
     "id": "1782102000762",
     "title": "我的第一篇文章",
     "date": "2026-06-22",
